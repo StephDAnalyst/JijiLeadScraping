135ed22df84e2b3dd468658dd7ebf7c95f03f1d9
Attamah Chikaodili <144041046+StephDAnalyst@users.noreply.github.com>
2023-10-24 15:18:31 +0100

    cookies.py

--- cookies.py
+++ cookies.py
@@ -25,11 +25,11 @@ email_phone_input.click()
 
 # Input the email or phone number
 emailinput = wait.until(EC.presence_of_element_located((By.XPATH, "//input[contains(@class, 'qa-login-field')]")))
-emailinput.send_keys("stephniechikaodili@gmail.com")
+emailinput.send_keys("Your_email")
 
 # Locate the password input field and input the password
 password_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@type='password']")))
-password_input.send_keys("0813636Li")
+password_input.send_keys("Your_password")
 
 # Locate and click the "Log in" button
 login_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'SIGN IN')]")))
